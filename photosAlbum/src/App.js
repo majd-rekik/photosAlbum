75b08f95b697e8a4f630aee565c838e5074b040c
majdReki <majd.rekik@gmail.com>
2020-12-25 23:13:22 +0100

    personal info and select different Titel added

--- photosAlbum/src/App.js
+++ photosAlbum/src/App.js
@@ -21,6 +21,11 @@ import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 import axios from "axios";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -51,6 +56,10 @@ const App = () => {
   const [urlImage, setUrlImage] = useState("");
   const [label, setLabel] = useState("");
   const [open, setOpen] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
     setLoading(true);
@@ -61,7 +70,7 @@ const App = () => {
       .finally(() => setLoading(false));
   }, []);
 
-  if (data) console.log(data.data);
+  /*if (data) console.log(data.data);*/
 
   const photoBefor = (event) => {
     if (data) {
@@ -82,141 +91,173 @@ const App = () => {
     }
   };
 
-  const photoAdd = (event) => {
-    setAddClicked(true);
+  const handleChange = (event) => {
+    setTitle(event.target.value);
   };
-  const photoSup = (event) => {
-    if (data.data.length !== 1) {
-      data.data.splice(photoId, 1);
-      photoBefor();
-    } else {
-      setOpen(true);
-    }
-  };
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setOpen(false);
-  };
-  const saleh = (event) => {
-    data.data.push({
-      picture: urlImage,
-      lastName: label,
-    });
-    setAddClicked(false);
-    setUrlImage("");
-    setLabel("");
-  };
-  return (
-    <div className="App">
-      {loading && "Loading..."}
-      {data && (
-        <div>
-          <header className="App-header">
-            <div className="majd">Data Base info </div>
-          </header>
-          <div
-            className="content"
-            style={{
-              backgroundImage: `url(${data.data[photoId].picture})`,
-              backgroundSize: "cover",
-              overflow: "hidden",
-            }}
-          >
-            <Card className="card">
-              <CardMedia
-                className="App-logo"
-                image={data.data[photoId].picture}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  <strong>{`Title: `}</strong>
-                  {data.data[photoId].title}
-                </Typography>
-                <Typography gutterBottom variant="h5" component="h2">
-                  <strong>{`First Name: `}</strong>
-                  {data.data[photoId].firstName}
-                </Typography>
-                <Typography gutterBottom variant="h5" component="h2">
-                  <strong>{`Last Name: `}</strong>
-                  {data.data[photoId].lastName}
-                </Typography>
-                <Typography gutterBottom variant="h5" component="h2">
-                  <strong>{`Email: `}</strong>
-                  {data.data[photoId].email}
-                </Typography>
-              </CardContent>
-              <CardActions className="Befor">
-                <IconButton onClick={photoBefor} aria-label="add an alarm">
-                  <NavigateBeforeIcon fontSize="large" />
-                </IconButton>
-                {addClicked === false && (
-                  <div>
-                    <IconButton
-                      onClick={photoAdd}
-                      color="primary"
-                      aria-label="upload picture"
-                      component="span"
-                    >
-                      <AddToPhotosIcon fontSize="large" />
-                    </IconButton>
-                    <IconButton color="secondary" onClick={photoSup}>
-                      <HighlightOffIcon fontSize="large" />
-                    </IconButton>
-                  </div>
-                )}
-                {addClicked === true && (
-                  <form onSubmit={saleh} noValidate autoComplete="off">
-                    <TextField
-                      id="filled-basic"
-                      label="Url image"
-                      value={urlImage}
-                      onChange={(event) => setUrlImage(event.target.value)}
-                    />
-                    <TextField
-                      id="filled-basic"
-                      label="firstName"
-                      value={label}
-                      onChange={(event) => setLabel(event.target.value)}
-                    />
-                    <TextField
-                      id="filled-basic"
-                      label="lastName"
-                      value={label}
-                      onChange={(event) => setLabel(event.target.value)}
-                    />
-                    <TextField
-                      id="filled-basic"
-                      label="Email"
-                      value={label}
-                      onChange={(event) => setLabel(event.target.value)}
-                    />
 
-                    <Button type="submit" variant="contained" color="secondary">
-                      Add
-                    </Button>
-                  </form>
-                )}
-                <IconButton
-                  aria-label="add to shopping cart"
-                  onClick={photoNext}
-                >
-                  <NavigateNextIcon fontSize="large" />
-                </IconButton>
-              </CardActions>
-            </Card>
-            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-              <Alert onClose={handleClose} severity="error">
-                There is only one photo left
-              </Alert>
-            </Snackbar>
+    const photoAdd = (event) => {
+      setAddClicked(true);
+    };
+    const photoSup = (event) => {
+      if (data.data.length !== 1) {
+        data.data.splice(photoId, 1);
+        photoBefor();
+      } else {
+        setOpen(true);
+      }
+    };
+    const handleClose = (event, reason) => {
+      if (reason === "clickaway") {
+        return;
+      }
+      setOpen(false);
+    };
+    const saleh = (event) => {
+      data.data.push({
+        picture: urlImage,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        title: title,
+      });
+      setAddClicked(false);
+      setUrlImage("");
+      setLabel("");
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setTitle("");
+    };
+    return (
+      <div className="App">
+        {loading && "Loading..."}
+        {data && (
+          <div>
+            <header className="App-header">
+              <div className="majd">Data Base info </div>
+            </header>
+            <div
+              className="content"
+              style={{
+                backgroundImage: `url(${data.data[photoId].picture})`,
+                backgroundSize: "cover",
+                overflow: "hidden",
+              }}
+            >
+              <Card className="card">
+                <CardMedia
+                  className="App-logo"
+                  image={data.data[photoId].picture}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    <strong>{`Title: `}</strong>
+                    {data.data[photoId].title}
+                  </Typography>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    <strong>{`First Name: `}</strong>
+                    {data.data[photoId].firstName}
+                  </Typography>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    <strong>{`Last Name: `}</strong>
+                    {data.data[photoId].lastName}
+                  </Typography>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    <strong>{`Email: `}</strong>
+                    {data.data[photoId].email}
+                  </Typography>
+                </CardContent>
+                <CardActions className="Befor">
+                  <IconButton onClick={photoBefor} aria-label="add an alarm">
+                    <NavigateBeforeIcon fontSize="large" />
+                  </IconButton>
+                  {addClicked === false && (
+                    <div>
+                      <IconButton
+                        onClick={photoAdd}
+                        color="primary"
+                        aria-label="upload picture"
+                        component="span"
+                      >
+                        <AddToPhotosIcon fontSize="large" />
+                      </IconButton>
+                      <IconButton color="secondary" onClick={photoSup}>
+                        <HighlightOffIcon fontSize="large" />
+                      </IconButton>
+                    </div>
+                  )}
+                  {addClicked === true && (
+                    <form onSubmit={saleh} noValidate autoComplete="off">
+                      <TextField
+                        id="filled-basic"
+                        label="Url image"
+                        value={urlImage}
+                        onChange={(event) => setUrlImage(event.target.value)}
+                      />
+                      <TextField
+                        id="filled-basic"
+                        label="firstname"
+                        value={firstName}
+                        onChange={(event) => setFirstName(event.target.value)}
+                      />
+                      <TextField
+                        id="filled-basic"
+                        label="lastname"
+                        value={lastName}
+                        onChange={(event) => setLastName(event.target.value)}
+                      />
+                      <TextField
+                        id="filled-basic"
+                        label="Email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                      />
+                      <FormControl >
+                        <InputLabel id="select-title">Title</InputLabel>
+                        <Select
+                          labelId="select-title"
+                          id="select-title"
+                          value={title}
+                          onChange={handleChange}
+                        >
+                          <MenuItem value={"mr"}>mr</MenuItem>
+                          <MenuItem value={"miss"}>miss</MenuItem>
+                          <MenuItem value={"ms"}>ms</MenuItem>
+                          <MenuItem value={"mrs"}>mrs</MenuItem>
+                        </Select>
+                      </FormControl>
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        color="secondary"
+                      >
+                        Add
+                      </Button>
+                    </form>
+                  )}
+                  <IconButton
+                    aria-label="add to shopping cart"
+                    onClick={photoNext}
+                  >
+                    <NavigateNextIcon fontSize="large" />
+                  </IconButton>
+                </CardActions>
+              </Card>
+              <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+              >
+                <Alert onClose={handleClose} severity="error">
+                  There is only one photo left
+                </Alert>
+              </Snackbar>
+            </div>
+            <footer className="App-footer">copywrites: Majd.Rekik</footer>
           </div>
-          <footer className="App-footer">copywrites: Majd.Rekik</footer>
-        </div>
-      )}
-    </div>
-  );
-};
+        )}
+      </div>
+    );
+  };
 
 export default App;
